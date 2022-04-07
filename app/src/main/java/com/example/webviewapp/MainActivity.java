e9bf449889a818ac9928ea90d39081ee110296b1
b21willu <b21willu@student.his.se>
2022-04-07 14:03:11 +0200

    Called `showExternalWebPage()` and `showInternalWebPage()` when menu dropdown is clicked

--- app/src/main/java/com/example/webviewapp/MainActivity.java
+++ app/src/main/java/com/example/webviewapp/MainActivity.java
@@ -91,11 +91,13 @@ public class MainActivity extends AppCompatActivity {
 
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_external_web) {
+            showExternalWebPage();
             Log.d("==>","Will display external web page");
             return true;
         }
 
         if (id == R.id.action_internal_web) {
+            showInternalWebPage();
             Log.d("==>","Will display internal web page");
             return true;
         }
